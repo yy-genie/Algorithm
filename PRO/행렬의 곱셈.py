a1a198726b1efda746d85941aa9aba8f3e4665e4
YYJ <eu9309@naver.com>
2026-02-13 09:34:51 +0900

    행렬의 곱셈

--- PRO/행렬의 곱셈.py
+++ PRO/행렬의 곱셈.py
@@ -1,19 +1,23 @@
 def solution(arr1, arr2):
+    # arr2 전치행렬
     arr2 = matrix_T(arr2)
     N = len(arr1)
     M = len(arr2)
+    # 연산 결과 넣을 answer 리스트 생성
     answer = [[0]*M for _ in range(N)]
-    # answer[i][j]에 원소 넣기
+    # answer[x][y]에 원소 넣기
     for x in range(N):
         e1 = arr1[x]
         for y in range(M):
             e2 = arr2[y]
             element = 0
+            # 곱 연산
             for z in range(len(arr1[0])):
                 element += e1[z] * e2[z]
             answer[x][y] = element
     return answer
 
+# 전치행렬 함수 구현
 def matrix_T(arr):
     new_arr = [[0]*(len(arr)) for _ in range(len(arr[0]))]
     for i in range(len(arr[0])):
