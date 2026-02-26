e81c5acaf01a6a8e62aa6b44fe31ec72e727e892
YJ <eu9309@naver.com>
2026-02-26 17:41:12 +0900

    활주로 건설

--- SWEA/A형/4014_활주로 건설.py
+++ SWEA/A형/4014_활주로 건설.py
@@ -1,5 +1,6 @@
 T = int(input())
 
+# 전치행렬 구현
 def matrix_T(arr):
     new_arr = [[0]*(len(arr)) for _ in range(len(arr[0]))]
     for i in range(len(arr[0])):
@@ -7,52 +8,61 @@ def matrix_T(arr):
             new_arr[i][j] = arr[j][i]
     return new_arr
 
+# 한 줄씩 경사로 가능/불가능 여부 체크
 def check(path):
-    rtemp = path[0]
-    ltemp = path[0]
-    result = False
-    answer = 0
-    for j in range(1, N-X+1):
-        if path[j] == rtemp:
-            continue
-        elif path[j] == rtemp-1:
-            # 오른쪽으로 길이 X만큼 있는지 확인
-            for d in range(X):
-                if path[j+d] == rtemp-1:
-                    result = True
-                    continue
-                else:
-                    result = False
-                    break
-            if result:
-                answer += 1
-                rtemp = path[j]
-        elif path[j]-1 == ltemp:
-            # 왼쪽부터 길이 X만큼 있는지 확인
-            cnt = 0
-            for d in range(j+1):
-                if path[d] == path[j]-1:
-                    cnt += 1
-                    result = True
-                else:
-                    result = False
-                    break
-            if cnt >= X:
-                answer += 1
-                ltemp = path[j]
-        else:
-            continue
-    return answer
+    # 경사로 설치 여부 표시할 visited 배열
+    visited = [False]*N
     
+    for i in range(N-1):
+        # 높이 같을 때 다음 인덱스로
+        if path[i] == path[i+1]:
+            continue
+
+        # 높이 차이 1 초과면 불가능
+        if abs(path[i] - path[i+1]) > 1:
+            return 0
+        
+        # 내리막 경사로
+        if path[i]-1 == path[i+1]:
+            h = path[i+1]
+            # i+1 부터 X칸 확인
+            if i+X >= N:  # 인덱스 범위 확인
+                return 0
+            for j in range(i+1, i+X+1):  # i+1부터 i+X까지
+                if path[j] != h or visited[j]:
+                    return 0
+            # 경사로 설치 표시
+            for j in range(i+1, i+X+1):
+                visited[j] = True
+
+        # 오르막 경사로
+        elif path[i]+1 == path[i+1]:
+            h = path[i]
+            # i부터 왼쪽으로 X칸 확인
+            if i-X+1 < 0:
+                return 0
+            for j in range(i, i-X, -1):
+                if path[j] != h or visited[j]:
+                    return 0
+            # 경사로 설치 표시
+            for j in range(i, i-X, -1):
+                visited[j] = True
+
+    return 1
+
 for tc in range(1, T+1):
     N, X = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
     total = 0
 
+    # 테케 line 한 줄씩 검사
     for path in arr:
+        # 해당 줄이 가능할 경우 1리턴 받아서 누적합
         total += check(path)
 
+    # 세로의 경우 전치행렬 활용
     arr2 = matrix_T(arr)
+    # 가로와 동일 방식
     for path in arr2:
         total += check(path)
 
