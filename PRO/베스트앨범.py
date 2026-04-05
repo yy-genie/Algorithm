ab6225488ad3804cfe36c957e6e87050c65d5295
YYJ <eu9309@naver.com>
2026-04-05 22:14:48 +0900

    수정

--- PRO/베스트앨범.py
+++ PRO/베스트앨범.py
@@ -1,27 +1,39 @@
 def solution(genres, plays):
-    from collections import defaultdict
+    genre_total = {}   # 장르별 총 재생 수
+    genre_songs = {}   # 장르별 노래 리스트
     
-    # 장르별 총 재생 수
-    genre_total = defaultdict(int)
-    
-    # 장르별 노래 리스트 (재생 수, 고유번호)
-    genre_songs = defaultdict(list)
-    
-    for i, (g, p) in enumerate(zip(genres, plays)):
+    # 데이터
+    for i in range(len(genres)):
+        g = genres[i]
+        p = plays[i]
+        
+        # 총합
+        if g not in genre_total:
+            genre_total[g] = 0
         genre_total[g] += p
+        
+        # 노래 리스트
+        if g not in genre_songs:
+            genre_songs[g] = []
         genre_songs[g].append((p, i))
     
-    # 장르를 총 재생 수 기준으로 정렬
-    sorted_genres = sorted(genre_total, key=lambda x: genre_total[x], reverse=True)
+    # 장르 정렬 (총 재생 수 기준)
+    sorted_genres = sorted(
+        genre_total.keys(),
+        key=lambda x: genre_total[x],
+        reverse=True
+    )
     
     answer = []
     
-    # 각 장르별 상위 2개 선택
+    # 각 장르별 top 2
     for g in sorted_genres:
-        # 재생 수 내림차순 고유번호 오름차순
-        songs = sorted(genre_songs[g], key=lambda x: (-x[0], x[1]))
+        songs = sorted(
+            genre_songs[g],
+            key=lambda x: (-x[0], x[1])
+        )
         
-        # 최대 2개 선택
-        answer.extend([idx for _, idx in songs[:2]])
+        for i in range(min(2, len(songs))):
+            answer.append(songs[i][1])
     
     return answer
